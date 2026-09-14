e68e45be8b3d6c1100439402f32d118835cc3dfa
curibarri <cristian.uribarri.m@gmail.com>
2026-09-14 15:37:06 -0300

    Agrega talleres CDN y carrusel 4:5 con continuidad de marca

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 // Bump the version whenever a published application file changes.
-const VERSION='2026-09-11-5';
+const VERSION='2026-09-14-3';
 const ROOT=self.registration.scope;
 const PREFIX='mantenedor-graficas:'+new URL(ROOT).pathname+':';
 const CACHE=PREFIX+VERSION;
